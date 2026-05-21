fa3e9d511ecbbfdeadd46b831a5c567f68bf6eb9
juragati <juragati@gmail.com>
2026-05-21 17:42:49 +0700

    Delete apps directory

--- apps/api/prisma/migrations/20260216115239_tambah_bukti_transfer/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "Order" ADD COLUMN     "buktiDp" TEXT,
-ADD COLUMN     "buktiLunas" TEXT;
--- apps/api/src/routes/profitRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { getProfitData } from '../controllers/profitController.js';
-
-const router = express.Router();
-router.get('/', getProfitData);
-
-export default router;
